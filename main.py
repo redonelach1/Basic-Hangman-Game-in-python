150e16e34eb4a8e8e576712e1f95298460d413da
Redone Lachgar <rednahdawi@gmail.com>
2022-03-15 16:13:52 +0100

    Update main.py
    Fixed The visuals.

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ from hangman_visual import lives_visual_dict
 word = random.choice(words)
 length = len(word)
 #print(word)
-print(f"The Word has {length} letters")
+#print(f"The Word has {length} letters")
 letter_lst=list(word)
 print("* "*length)
 inpt=["*"]*length
-lives=6
+lives=7
 Lose_Flag=False
 def allocate(index,test):
     inpt[index]=f"{test}"
 
-print(lives_visual_dict[6-lives])
+print(lives_visual_dict[lives])
 for i in range(length):
     test = input("\na letter:")
     Flag= False
-    while(Flag!=True):
+    while(Flag!=True and lives > 0):
         try:
             index = letter_lst.index(test)
         except ValueError:
-            print(lives_visual_dict[7-lives])
-            lives=lives-1
-            test = input(f"Wrong letter ({lives+1} lives left): ")
+            test = input(f"Wrong letter ({lives} lives left): ")
+            lives = lives - 1
+            print(lives_visual_dict[lives])
         else:
             Flag = True
         if (lives==0):
@@ -36,8 +36,6 @@ for i in range(length):
     for i in range(length):
         print(inpt[i], end="")
 if (Lose_Flag==True):
-    print("You Lost The Man died")
+    print("You Lost The Man Has died")
 else:
     print("\nWell Done You Found The Name :), The Man was released")
-  
-#I Have No Damn Idea how this code works, but it does work. (atleast on pycharm xD)
